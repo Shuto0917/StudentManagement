15e168f880347c16fbc6842d1d6e94cbc0c76f0d
Shuto Ishii <link09078002436@outlook.jp>
2024-09-29 19:08:42 +0900

    第10回演習課題

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,16 +1,10 @@
 package raisetech.student.management;
 
 import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -26,24 +20,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/student")
-	public List<Student> getAllStudents() {
-		return repository.getAllStudent();
-	}
-
-	@PostMapping("/student")
-	public void registerStudent(String name, int age) {
-		repository.registerStudent(name, age);
-
-	}
-
-	@PatchMapping("/student")
-	public void updateStudent(String name, int age) {
-		repository.updateStudent(name,age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name) {
-		repository.deleteStudent(name);
+	@GetMapping("/studentCourseList")
+	public List<StudentCourses> getStudentCourseList() {
+		return repository.searchStudentCourse();
 	}
 }
