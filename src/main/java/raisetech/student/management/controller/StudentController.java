83eefd8c95312493cba89f6f5b3769125ae1f612
Shuto Ishii <link09078002436@outlook.jp>
2024-10-20 04:16:33 +0900

    第14回演習課題

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.student.management.controller.converter.StudentConverter;
@@ -12,7 +14,7 @@ import raisetech.student.management.data.StudentCourses;
 import raisetech.student.management.domain.StudentDetail;
 import raisetech.student.management.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
@@ -26,11 +28,12 @@ public StudentController(StudentService service, StudentConverter converter) {
 
 
   @GetMapping("/studentsList")
-  public List<StudentDetail> searchStudentsList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentCourses> studentCourses = service.searchStudentCourseList();
 
-    return converter.convertStudentDetails(students, studentCourses);
+    model.addAttribute("studentList", converter.convertStudentDetails(students,studentCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentCourseList")
