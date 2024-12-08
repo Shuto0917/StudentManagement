706b01edec16b3d828833f4b62d5dc75de3bfbc3
Shuto Ishii <09078002436@docomo.ne.jp>
2024-12-08 15:50:40 +0900

    テンプレート追加

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,55 +19,55 @@ import raisetech.student.management.service.StudentService;
 @Controller
 public class StudentController {
 
-  private StudentService service;
-  private StudentConverter converter;
+    private StudentService service;
+    private StudentConverter converter;
 
-@Autowired
-public StudentController(StudentService service, StudentConverter converter) {
-  this.service = service;
-  this.converter = converter;
-}
-
-
-  @GetMapping("/studentsList")
-  public String getStudentList(Model model) {
-    List<Student> students = service.searchStudentList();
-    List<StudentCourses> studentCourses = service.searchStudentCourseList();
-
-    model.addAttribute("studentList", converter.convertStudentDetails(students,studentCourses));
-    return "studentList";
-  }
-
-  @GetMapping("/Student/{id}")
-  public String getStudent(@PathVariable int id, Model model) {
-    StudentDetail studentDetail = service.searchStudent(id);
-    model.addAttribute("studentDetail", studentDetail);
-    return  "updateStudent";
-  }
-
-  @GetMapping("/newStudent")
-  public String newStudent(Model model) {
-    StudentDetail studentDetail = new StudentDetail();
-    studentDetail.setStudentCourses(Arrays.asList(new StudentCourses()));
-    model.addAttribute("studentDetail", studentDetail);
-    return  "registerStudent";
-  }
-
-  @PostMapping("/registerStudent")
-  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if (result.hasErrors()) {
-      return "registerStudent";
+    @Autowired
+    public StudentController(StudentService service, StudentConverter converter) {
+        this.service = service;
+        this.converter = converter;
     }
-    service.registerStudent(studentDetail);
-    return "redirect:/studentsList";
-  }
 
-  @PostMapping("/updateStudent")
-  public String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if (result.hasErrors()) {
-      return "updateStudent";
+
+    @GetMapping("/studentsList")
+    public String getStudentList(Model model) {
+        List<Student> students = service.searchStudentList();
+        List<StudentCourses> studentCourses = service.searchStudentCourseList();
+
+        model.addAttribute("studentList", converter.convertStudentDetails(students, studentCourses));
+        return "studentList";
     }
-    service.updateStudent(studentDetail);
-    return "redirect:/studentsList";
-  }
+
+    @GetMapping("/student/{id}")
+    public String getStudent(@PathVariable int id, Model model) {
+        StudentDetail studentDetail = service.searchStudent(id);
+        model.addAttribute("studentDetail", studentDetail);
+        return "updateStudent";
+    }
+
+    @GetMapping("/newStudent")
+    public String newStudent(Model model) {
+        StudentDetail studentDetail = new StudentDetail();
+        studentDetail.setStudentCourses(Arrays.asList(new StudentCourses()));
+        model.addAttribute("studentDetail", studentDetail);
+        return "registerStudent";
+    }
+
+    @PostMapping("/registerStudent")
+    public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+        if (result.hasErrors()) {
+            return "registerStudent";
+        }
+        service.registerStudent(studentDetail);
+        return "redirect:/studentsList";
+    }
+
+    @PostMapping("/updateStudent")
+    public String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+        if (result.hasErrors()) {
+            return "updateStudent";
+        }
+        service.updateStudent(studentDetail);
+        return "redirect:/studentsList";
+    }
 }
