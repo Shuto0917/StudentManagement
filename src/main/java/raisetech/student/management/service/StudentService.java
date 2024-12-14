90135037d9a166aeb21b58c33ca980910b8334d0
Shuto Ishii <09078002436@docomo.ne.jp>
2024-12-14 20:12:41 +0900

    スタンダード第17回演習課題

--- src/main/java/raisetech/student/management/service/StudentService.java
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -2,6 +2,7 @@ package raisetech.student.management.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,47 +14,62 @@ import raisetech.student.management.repository.StudentRepository;
 @Service
 public class StudentService {
 
-  private StudentRepository repository;
+    private StudentRepository repository;
 
-  @Autowired
-  public StudentService(StudentRepository repository) {
-    this.repository = repository;
-  }
-
-  public List<Student> searchStudentList() {
-    return repository.search();
-
-  }
-
-  public StudentDetail searchStudent(int id) {
-    Student student = repository.searchStudent(id);
-    List<StudentCourses> studentsCourses = repository.searchStudentsCourses(Integer.parseInt(student.getId()));
-    StudentDetail studentDetail = new StudentDetail();
-    studentDetail.setStudent(student);
-    studentDetail.setStudentCourses(studentsCourses);
-    return studentDetail;
-  }
-
-  public List<StudentCourses> searchStudentCourseList() {
-    return repository.searchStudentsCoursesList();
-  }
-  @Transactional
-  public void registerStudent(StudentDetail studentDetail) {
-    repository.registerStudent(studentDetail.getStudent());
-    for (StudentCourses studentCourse : studentDetail.getStudentCourses()) {
-      studentCourse.setStudentId(studentDetail.getStudent().getId());
-      studentCourse.setCourseStartAt(LocalDateTime.now());
-      studentCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
-      repository.registerStudentCourses(studentCourse);
+    @Autowired
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
     }
-  }
 
+    public List<Student> searchStudentList() {
+        return repository.search();
 
-  @Transactional
-  public void updateStudent(StudentDetail studentDetail) {
-    repository.updateStudent(studentDetail.getStudent());
-    for (StudentCourses studentCourse : studentDetail.getStudentCourses()) {
-      repository.updateStudentCourses(studentCourse);
     }
-  }
+
+    public List<Student> searchCanceledStudents() {
+        return repository.searchCanceledStudents(); //キャンセルされた受講生を取得
+    }
+
+    public StudentDetail searchStudent(int id) {
+        Student student = repository.searchStudent(id);
+        List<StudentCourses> studentsCourses = repository.searchStudentsCourses(Integer.parseInt(student.getId()));
+        StudentDetail studentDetail = new StudentDetail();
+        studentDetail.setStudent(student);
+        studentDetail.setStudentCourses(studentsCourses);
+        return studentDetail;
+    }
+
+    public List<StudentCourses> searchStudentCourseList() {
+        return repository.searchStudentsCoursesList();
+    }
+
+    @Transactional
+    public void registerStudent(StudentDetail studentDetail) {
+        repository.registerStudent(studentDetail.getStudent());
+        for (StudentCourses studentCourse : studentDetail.getStudentCourses()) {
+            studentCourse.setStudentId(studentDetail.getStudent().getId());
+            studentCourse.setCourseStartAt(LocalDateTime.now());
+            studentCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
+            repository.registerStudentCourses(studentCourse);
+        }
+    }
+
+
+    @Transactional
+    public void updateStudent(StudentDetail studentDetail) {
+        repository.updateStudent(studentDetail.getStudent());
+        for (StudentCourses studentCourse : studentDetail.getStudentCourses()) {
+            repository.updateStudentCourses(studentCourse);
+        }
+    }
+
+    public void restoreStudent(int studentId) {
+        Student student = repository.searchStudent(studentId);
+
+        if (student != null && student.isDeleted()) {
+            student.setDeleted(false); //キャンセルを解除
+            repository.updateStudent(student);
+        }
+    }
 }
+
